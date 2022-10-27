f6cc7e687389b9768bfeb9bc80ae23232b668d19
Curtis Larson <quackware@gmail.com>
2022-10-27 23:23:27 +0200

    init

---
+++ tasks/download.ts
@@ -0,0 +1,18 @@
+const url = Deno.env.get("DOWNLOAD_URL");
+if (url === undefined) {
+  console.error("DOWNLOAD_URL is not provided");
+  Deno.exit();
+}
+
+async function download(downloadUrl: string) {
+  const file = Deno.makeTempFileSync();
+  const denoFile = Deno.openSync(file);
+
+  const stream = await fetch(downloadUrl).then((r) => r.body!);
+
+  await stream.pipeTo(denoFile.writable);
+
+  denoFile.close();
+}
+
+download(url).catch(console.error);
